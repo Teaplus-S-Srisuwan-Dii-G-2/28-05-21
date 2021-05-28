8673c9f60d8bf975808ec89a9b249a9a99f2975a
Sinmund-doom <Teaplus_s@cmu.ac.th>
2021-05-28 16:14:44 +0700

    commit4
    .....

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
-var newText_name = 'Sigmund_doom'
-document.getElementById('no').innerHTML = newText_name
+document.getElementById('ok').addEventListener('click',function(e)
+{
+    alert("Hello")
+})
