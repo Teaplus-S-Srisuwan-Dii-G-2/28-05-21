8d8990910e7ebec547a2dc1286905460ed43f145
Sinmund-doom <Teaplus_s@cmu.ac.th>
2021-05-28 16:56:35 +0700

    commit5
    .....

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.getElementById('ok').addEventListener('click',function(e)
 {
     alert("Hello")
 })
+
+document.getElementById('cancel').addEventListener('mousemove', function(e)
+{
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "("+x+","+y+")";
+    console.log(coor)
+    
+})
+
+
